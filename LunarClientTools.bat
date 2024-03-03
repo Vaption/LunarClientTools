836ee6813815114d94bcc9004a55daefa3a4716c
Vaption ✨ <153444810+Vaption@users.noreply.github.com>
2024-03-03 20:58:06 +0330

    license style fix

--- LunarClientTools.bat
+++ LunarClientTools.bat
@@ -4,14 +4,17 @@
 
 @rem MIT License
 @rem Copyright (c) 2024 Vaption ✨
+
 @rem Permission is hereby granted, free of charge, to any person obtaining a copy
 @rem of this software and associated documentation files (the "Software"), to deal
 @rem in the Software without restriction, including without limitation the rights
 @rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 @rem copies of the Software, and to permit persons to whom the Software is
 @rem furnished to do so, subject to the following conditions:
+
 @rem The above copyright notice and this permission notice shall be included in all
 @rem copies or substantial portions of the Software.
+
 @rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 @rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 @rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
